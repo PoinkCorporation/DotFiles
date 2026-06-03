a7541df8fdda45b143d768671d3e285a4bb74a0e
Poink corp. <125809913+PoinkCorporation@users.noreply.github.com>
2026-06-03 12:23:08 +0300

    Add files via upload

---
+++ config/nvim/init.lua
@@ -0,0 +1,3 @@
+require("config.lazy")
+require("config.keymappings")
+require("config.settings")
---
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "storm",           -- "storm", "moon", "night", "day"
+    transparent = false,
+    terminal_colors = true,
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd.colorscheme("tokyonight")
+  end,
+}
